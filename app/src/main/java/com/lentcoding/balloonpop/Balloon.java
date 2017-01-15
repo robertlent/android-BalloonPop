2b6e9091e0c14e3ff1cdee1984c032c772bf5ed9
Robert <robert.lent@outlook.com>
2017-01-15 12:19:14 -0500

    Initial commit

---
+++ app/src/main/java/com/lentcoding/balloonpop/Balloon.java
@@ -0,0 +1,96 @@
+package com.lentcoding.balloonpop;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+
+import com.lentcoding.balloonpop.utils.PixelHelper;
+
+@SuppressWarnings("unused")
+public class Balloon extends ImageView implements ValueAnimator.AnimatorUpdateListener, Animator.AnimatorListener {
+    private ValueAnimator mAnimator;
+    private BalloonListener mListener;
+    private boolean mPopped;
+
+    public Balloon(Context context) {
+        super(context);
+    }
+
+    public Balloon(Context context, int color, @SuppressWarnings("SameParameterValue") int rawHeight) {
+        super(context);
+        mListener = (BalloonListener) context;
+        this.setImageResource(R.drawable.balloon);
+        this.setColorFilter(color);
+
+        int rawWidth = rawHeight / 3;
+        int dpHeight = PixelHelper.pixelsToDp(rawHeight, context);
+        int dpWidth = PixelHelper.pixelsToDp(rawWidth, context);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(dpWidth, dpHeight);
+        setLayoutParams(params);
+    }
+
+    public void releaseBalloon(int screenHeight, int duration) {
+        mAnimator = new ValueAnimator();
+        mAnimator.setDuration(duration);
+        mAnimator.setFloatValues(screenHeight, 0f);
+        mAnimator.setInterpolator(new LinearInterpolator());
+        mAnimator.setTarget(this);
+        mAnimator.addListener(this);
+        mAnimator.addUpdateListener(this);
+        mAnimator.start();
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        if (!mPopped) {
+            mListener.popBalloon(this, false);
+        }
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator animation) {
+        setY((Float) animation.getAnimatedValue());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!mPopped && event.getAction() == MotionEvent.ACTION_DOWN) {
+            mListener.popBalloon(this, true);
+            mPopped = true;
+            mAnimator.cancel();
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public void setPopped(@SuppressWarnings("SameParameterValue") boolean popped) {
+        mPopped = popped;
+
+        if (popped) {
+            mAnimator.cancel();
+
+        }
+    }
+
+    public interface BalloonListener {
+        void popBalloon(Balloon balloon, boolean userTouch);
+    }
+}
